f8cd4027d71978e61ad159372ca3e7d779e70800
Michael Wirth <michael.wirth@mimacom.com>
2018-03-28 11:03:47 +0200

    fix typo

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,16 +1,38 @@
-import './vendor.ts';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
 
 import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Ng2Webstorage } from 'ngx-webstorage';
-
-import { JobroomSharedModule, UserRouteAccessService } from './shared';
-import { JobroomHomeModule } from './home';
-import { JobroomAdminModule } from './admin/admin.module';
+import { EffectsModule } from '@ngrx/effects';
+import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { JhiEventManager } from 'ng-jhipster';
+import { JhiBase64Service } from 'ng-jhipster/src/service/base64.service';
+import { CookieService } from 'ngx-cookie';
+import {
+    LocalStorageService,
+    Ng2Webstorage,
+    SessionStorageService
+} from 'ngx-webstorage';
 import { JobroomAccountModule } from './account/account.module';
-import { JobroomEntityModule } from './entities/entity.module';
+import { JobroomAdminModule } from './admin/admin.module';
+import { DEBUG_INFO_ENABLED, VERSION } from './app.constants';
+
+import { AppRoutingModule } from './app.routing.module';
 
 import { PaginationConfig } from './blocks/config/uib-pagination.config';
+import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
+import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
+import { CacheKeyInterceptor } from './blocks/interceptor/cache.interceptor';
+import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
+import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
+import { CandidateSearchModule } from './candidate-search/candidate-search.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { JobroomEntityModule } from './entities/entity.module';
+import { JobroomHomeModule } from './home';
+import { JobSearchModule } from './job-search/job-search.module';
 import {
     ActiveMenuDirective,
     ErrorComponent,
@@ -25,36 +47,14 @@ import {
     VersionService
 } from './layouts';
 
-import { AppRoutingModule } from './app.routing.module';
-import { JobSearchModule } from './job-search/job-search.module';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { DEBUG_INFO_ENABLED, VERSION } from './app.constants';
-import { CandidateSearchModule } from './candidate-search/candidate-search.module';
-import { DashboardModule } from './dashboard/dashboard.module';
+import { JobroomSharedModule, UserRouteAccessService } from './shared';
 import { CustomRouterStateSerializer, } from './shared/custom-router-state-serializer/custom-router-state-serializer';
-import { RouterStateSerializer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { reducers } from './shared/state-management/reducers/core.reducers';
+import './vendor.ts';
 
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from './blocks/interceptor/auth.interceptor';
-import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
-import { AuthExpiredInterceptor } from './blocks/interceptor/auth-expired.interceptor';
-import { ErrorHandlerInterceptor } from './blocks/interceptor/errorhandler.interceptor';
-import { JhiEventManager } from 'ng-jhipster';
-import { NotificationInterceptor } from './blocks/interceptor/notification.interceptor';
-import { CacheKeyInterceptor } from './blocks/interceptor/cache.interceptor';
-import { CookieService } from 'ngx-cookie';
-import { JhiBase64Service } from 'ng-jhipster/src/service/base64.service';
-import { TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-// FIXME
-import { Http } from '@angular/http';
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 
-export function translatePartialLoader(http: Http) {
+export function translatePartialLoader(http: HttpClient) {
     // todo: remove it after migrating to the latest ng-jhipster version
     return new TranslateHttpLoader(http, 'i18n/', `.json?version=${VERSION}`);
 }
@@ -102,7 +102,6 @@ if (DEBUG_INFO_ENABLED) {
         PaginationConfig,
         UserRouteAccessService,
         { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
-        //FIXME
         { provide: TranslateLoader, useFactory: translatePartialLoader, deps: [ HttpClient ] },
         { provide: RouterStateSerializer, useClass: CustomRouterStateSerializer },
         {
