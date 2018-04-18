2940c377c9cff77603bebf94fb710f9d73d3b9a9
Fabian Gotzen <fabian.gotzen@mimacom.com>
2018-04-18 11:12:28 +0200

    JR2-874 first setup of system-notifications component

---
+++ src/main/webapp/app/shared/input-components/System-notifications/system-notifications-component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'system-notifications',
+    templateUrl: './system-notifications-component.html',
+    styleUrls: ['./system-notifications-component.scss']
+})
+
+export class SystemNotificationsComponent {
+
+    constructor(){
+
+    }
+}
