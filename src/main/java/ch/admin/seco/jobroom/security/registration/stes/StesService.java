e39e3fc7530e095e223beee74956185eb6e8e0d2
Patrick D <patrick.dobler@gmail.com>
2018-06-14 17:19:16 +0200

    StesService verifyStesRegistrationData returns not a boolean but an Object (#603)

--- src/main/java/ch/admin/seco/jobroom/security/registration/stes/StesService.java
+++ src/main/java/ch/admin/seco/jobroom/security/registration/stes/StesService.java
@@ -1,17 +1,16 @@
 package ch.admin.seco.jobroom.security.registration.stes;
 
-import javax.validation.Valid;
-
+import ch.admin.seco.jobroom.web.rest.vm.RegisterJobseekerVM;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import ch.admin.seco.jobroom.web.rest.vm.RegisterJobseekerVM;
+import javax.validation.Valid;
 
 @FeignClient(name = "candidateservice", decode404 = true)
 public interface StesService {
 
     @PostMapping("/api/candidates/verify")
-    boolean verifyStesRegistrationData(@Valid @RequestBody RegisterJobseekerVM jobseekerDetails);
+    StesVerificationResult verifyStesRegistrationData(@Valid @RequestBody RegisterJobseekerVM jobseekerDetails);
 
 }
---
+++ src/main/java/ch/admin/seco/jobroom/security/registration/stes/StesVerificationResult.java
@@ -0,0 +1,15 @@
+package ch.admin.seco.jobroom.security.registration.stes;
+
+public class StesVerificationResult {
+
+    private boolean verified;
+
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
+    }
+
+}
