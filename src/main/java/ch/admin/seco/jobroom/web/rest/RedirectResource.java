409a5991d417d429bb9255cd78ba3153636a2c22
Patrick Dobler <patrick.dobler@gmail.com>
2018-06-21 15:00:43 +0200

    fix bugs

--- src/main/java/ch/admin/seco/jobroom/web/rest/RedirectResource.java
+++ src/main/java/ch/admin/seco/jobroom/web/rest/RedirectResource.java
@@ -1,11 +1,11 @@
 package ch.admin.seco.jobroom.web.rest;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/api/redirect")
 public class RedirectResource {
 
