85712127e29dc6b642ec69a98cf124608082eddb
Patrick Dobler <patrick.dobler@gmail.com>
2018-07-05 01:23:22 +0200

    feature: log the saml assertion if there is an exception

--- src/main/java/ch/admin/seco/jobroom/security/saml/infrastructure/dsl/ImprovedSAMLLogger.java
+++ src/main/java/ch/admin/seco/jobroom/security/saml/infrastructure/dsl/ImprovedSAMLLogger.java
@@ -1,11 +1,14 @@
 package ch.admin.seco.jobroom.security.saml.infrastructure.dsl;
 
+import org.opensaml.ws.message.encoder.MessageEncodingException;
+import org.opensaml.xml.util.XMLHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.saml.context.SAMLMessageContext;
 import org.springframework.security.saml.log.SAMLDefaultLogger;
+import org.springframework.security.saml.util.SAMLUtil;
 
 public class ImprovedSAMLLogger extends SAMLDefaultLogger {
 
@@ -13,12 +16,23 @@ public class ImprovedSAMLLogger extends SAMLDefaultLogger {
 
     @Override
     public void log(String operation, String result, SAMLMessageContext context, Authentication a, Exception e) {
-        if (LOGGER.isTraceEnabled()) {
-            super.setLogMessages(true);
-        } else {
-            super.setLogMessages(false);
-        }
         super.log(operation, result, context, a, e);
+        if (e != null) {
+            LOGGER.info("Failing Saml Assertion: {}", extractSamlAssertion(context));
+        }
     }
 
+    private String extractSamlAssertion(SAMLMessageContext context) {
+        try {
+            if (context.getInboundSAMLMessage() != null) {
+                return XMLHelper.nodeToString(SAMLUtil.marshallMessage(context.getInboundSAMLMessage()));
+            }
+            if (context.getOutboundSAMLMessage() != null) {
+                return XMLHelper.nodeToString(SAMLUtil.marshallMessage(context.getOutboundSAMLMessage()));
+            }
+        } catch (MessageEncodingException e1) {
+            LOGGER.warn("Error marshaling message during logging", e1);
+        }
+        return "";
+    }
 }
